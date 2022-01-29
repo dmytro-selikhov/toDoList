5f8561eba57115f2c1506ceee00997584727739f
Dmytro Selikhov <dmytro.selikhov@gmail.com>
2022-01-29 22:22:54 +0200

    Added some code

---
+++ app.js
@@ -0,0 +1,16 @@
+// Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+// Event Listeners
+todoButton.addEventListener('click', addTodo);
+
+
+// Functions
+
+function addTodo(event) {
+  //Prevent form from submitting
+  event.preventDefalt();
+  console.log('Hello!');
+}
