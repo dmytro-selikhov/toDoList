0bcc24974db4a20cdd9fffd82f79b88a404db9e9
Dmytro Selikov <dmytro.selikhov@gmail.com>
2022-02-03 15:19:17 +0200

    added little js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 // Selectors
-const todoInput = document.querySelector('.todo-input');
-const todoButton = document.querySelector('.todo-button');
-const todoList = document.querySelector('.todo-list');
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
 
 // Event Listeners
-todoButton.addEventListener('click', addTodo);
+todoButton.addEventListener("click", addTodo);
 
 
 // Functions
@@ -12,5 +12,12 @@ todoButton.addEventListener('click', addTodo);
 function addTodo(event) {
   //Prevent form from submitting
   event.preventDefalt();
-  console.log('Hello!');
+  // Todo DIV
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+  // Create LI
+  const newTodo = document.createElement('li');
+  newTodo.innerText = 'hey';
+  newTodo.classList.add('todo-item');
+  todoDiv.appendChild(newTodo);
 }
